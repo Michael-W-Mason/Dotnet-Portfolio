1c9477ef9f907bbeb17754ad1008cced0549debf
Michael Mason <mwmason1998@gmail.com>
2022-08-24 11:08:56 -0400

    Integrated Full Page JS

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+new fullpage('#fullpage', {
+    autoscroll: true,
+    navigation: true,
+    sectionsColor: ['#f5f5f5', '#686464', '#f5f5f5', '#686464']
+})
